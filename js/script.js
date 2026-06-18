4ec4d926e70b9e615caa403ef4e2f4aae9a313a7
waverleypaul <wavepaj@gmail.com>
2026-06-18 14:41:53 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll("nav a");
 
+  window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+      const top = section.offsetTop;
+      if (pageYOffset >= top - 120) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach(link => {
+      link.classList.remove("active");
+      if (link.getAttribute("href") === "#" + current) {
+        link.classList.add("active");
+      }
+    });
+  });
