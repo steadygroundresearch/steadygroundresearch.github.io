4fd4cfd941c207ae88d7a04eb872c71e4367c98e
steadygroundresearch <294260259+steadygroundresearch@users.noreply.github.com>
2026-09-22 16:22:57 -0400

    update script.js

--- js/script.js
+++ js/script.js
@@ -858,7 +858,7 @@ if ('serviceWorker' in navigator) {
   const closeBtn = document.getElementById('pdf-modal-close');
 
   function openPdfModal(src, title){
-    frame.src = src + '#view=FitH';
+    frame.src = src ;
     titleEl.textContent = title || '';
     modal.classList.add('active');
   }
